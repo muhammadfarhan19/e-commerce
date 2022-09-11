8a72b9434d24184aa0fba050729fc5e902a855aa
muhammadfarhan19 <hamidie.farhan@gmail.com>
2022-09-11 14:47:27 +0700

    add setting bar

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const navBar = document.querySelector('.left-side');
-const navLink = document.querySelectorAll('.left-side-link');
-const suggestionCard = document.querySelectorAll('.card');
+const sideBar = document.querySelector(".left-side");
+const content = document.querySelector(".content");
+const display = document.querySelector(".display");
+const navLink = document.querySelectorAll(".left-side-link");
+const profile = document.querySelector('.display-profile');
+const setting = document.querySelector('.display-setting');
+const suggestionCard = document.querySelectorAll(".card");
+const navBarTitle = document.querySelector(".navbar-title");
+const closeDisplay = document.querySelector(".fa-close");
 
-navBar.addEventListener('click', (e) => {
+
+sideBar.addEventListener('click', (e) => {
   if (e.target.className == 'left-side-link') {
     navLink.forEach(el => {
       el.classList.remove('active');
@@ -11,8 +18,19 @@ navBar.addEventListener('click', (e) => {
   }
 })
 
-suggestionCard.forEach(el => {
-  el.addEventListener('mouseover', () => {
-    
+navLink.forEach((el) => {
+  el.addEventListener('click', (e) => {
+
+
+    if (e.target.textContent == 'Setting') {
+      navBarTitle.innerHTML = 'Setting';
+      profile.classList.add('d-none');
+      setting.classList.remove('d-none');
+    }
+    if (e.target.textContent == 'Profile') {
+      navBarTitle.innerHTML = 'Profile';
+      profile.classList.remove('d-none');
+      setting.classList.add('d-none');
+    }
   })
-})
+});
